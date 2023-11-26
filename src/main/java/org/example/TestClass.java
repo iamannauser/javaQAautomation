58767c6b9b30cc50b018f88d725059b35e0a2863
iamannauser <anna.lushchan@gmail.com>
2023-11-26 22:40:32 +0200

    initial commit;

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -5,7 +5,7 @@ import com.github.javafaker.Faker;
 public class TestClass {
     public static void main(String[] args) {
 
-        System.out.println(Faker.instance().address().fullAddress());
+        System.out.println(Faker.instance().date().birthday(18, 60));
 
 
     }
