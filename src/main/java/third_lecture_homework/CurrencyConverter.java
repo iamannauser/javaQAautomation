122f78e7ce8d8991f0186447c07b3df1b1950f80
iamannauser <anna.lushchan@gmail.com>
2023-12-04 04:28:49 +0200

    Random fruit and random vegetable;

--- src/main/java/third_lecture_homework/CurrencyConverter.java
+++ src/main/java/third_lecture_homework/CurrencyConverter.java
@@ -3,12 +3,10 @@ package third_lecture_homework;
 public class CurrencyConverter {
     private double commission;
     private double exchangeRate;
-
     public CurrencyConverter(double commission, double exchangeRate) {
         this.commission = commission;
         this.exchangeRate = exchangeRate;
     }
-
     private double convertUsdToUah(double amountInUsd) {
         return amountInUsd * exchangeRate;
     }
