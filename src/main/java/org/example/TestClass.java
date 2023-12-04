6b7a273b89e41623aaff27e488fde2118727ae51
iamannauser <anna.lushchan@gmail.com>
2023-12-04 04:24:06 +0200

    Random fruit and random vegetable;

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -1,14 +1,23 @@
 package org.example;
 
-import com.github.javafaker.Faker;
-
 public class TestClass {
     public static void main(String[] args) {
 
-        System.out.println(Faker.instance().food().fruit());
-        System.out.println(Faker.instance().food().vegetable());
 
 
+
+
+
+//        Cat cat1 = new Cat();
+//        Cat cat2 = new Cat(name: "Leo");
+//        Cat cat3 = new Cat(name: "Leo", age: 1);
+
+
+//        Cat newCat = new Cat(name "Leo", age "1");
+
+//        System.out.println(Faker.instance().food().fruit());
+//        System.out.println(Faker.instance().food().vegetable());
     }
+
 
 }
---
+++ src/main/java/third_lecture_homework/CurrencyConverter.java
@@ -0,0 +1,23 @@
+package third_lecture_homework;
+
+public class CurrencyConverter {
+    private double commission;
+    private double exchangeRate;
+
+    public CurrencyConverter(double commission, double exchangeRate) {
+        this.commission = commission;
+        this.exchangeRate = exchangeRate;
+    }
+
+    private double convertUsdToUah(double amountInUsd) {
+        return amountInUsd * exchangeRate;
+    }
+    private double calculateCommission(double amount) {
+        return amount * commission;
+    }
+    public double calculateTotalAmount(double amountInUsd) {
+        double convertedAmount = convertUsdToUah(amountInUsd);
+        double commissionAmount = calculateCommission(convertedAmount);
+        return convertedAmount + commissionAmount;
+    }
+}
